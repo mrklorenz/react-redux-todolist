94eb3fc5348fe8729a3e59e248111c6610a6e8eb
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-17 22:41:22 +0800

    [ReactRedux][Mark] feat: Added constants.js file for initial state

---
+++ src/common/constants/constants.js
@@ -0,0 +1,22 @@
+export const initialTodoList = [
+        {
+            id: "cc33dc26-61b0-406b-99dd-b8825dd2ceec",
+            text: "Clean the house",
+            done: false
+        },
+        {
+            id: "cc5432c26-61b0-406b-99dd-b8825dd2ceec",
+            text: "Finish homework",
+            done: false
+        },
+        {
+            id: "cc53dc26-61b0-406b-99dd-3231sdsddasd",
+            text: "Finish EOD and ORID",
+            done: false
+        },
+        {
+            id: "cc53dc2s6-61b0-406b-3214-b8825dd2ceec",
+            text: "Eat Dinner",
+            done: false
+        }
+    ];
