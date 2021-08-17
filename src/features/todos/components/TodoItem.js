0fed2790b3d001fb9a2b426592f5fd6257d5f41f
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-18 00:53:14 +0800

    [ReactRedux][Mark] feat: Added event on text div

--- src/features/todos/components/TodoItem.js
+++ src/features/todos/components/TodoItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {selectTodoItemId} from '../reducers/todosSlice';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {ToggleTodo} from '../reducers/todosSlice';
 
 function TodoItem(props) {
 
     const todo = useSelector(state => selectTodoItemId(state, props.id));
 
+    const dispatch = useDispatch();
+
+    function handleToggle(event){
+        dispatch(ToggleTodo(event.target.value));
+    }
+
     return (
-        <div>
+        <div onClick={handleToggle}>
             {todo.text}
         </div>
     );
