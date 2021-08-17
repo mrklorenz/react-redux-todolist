b0fe4256ee7992041bde24f260f827d195d9ab95
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-17 11:03:49 +0800

    [TodoList][Mark] chore: Added todo list project

---
+++ src/features/components/TodoGroup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+function TodoGroup(props) {
+    return (
+        <div>
+            TodoGroup
+            <TodoItem></TodoItem>
+        </div>
+    );
+}
+
+export default TodoGroup;
---
+++ src/features/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+TodoItem.propTypes = {
+    
+};
+
+function TodoItem(props) {
+    return (
+        <div>
+            todoitem
+        </div>
+    );
+}
+
+export default TodoItem;
