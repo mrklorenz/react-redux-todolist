76760de9772348ed58ec173f3a904ad63a2f5ff2
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-19 01:05:35 +0800

    [ReactRedux][Mark] feat: Add gettodos api to todogroup.js

--- src/features/todos/components/TodoGroup.js
+++ src/features/todos/components/TodoGroup.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TodoItem from './TodoItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {selectTodoIds} from "../reducers/todosSlice";
 import "../styles/ToDoGroup.css";
+import {getToDos} from '../../api/todosapi';
+import {AddToDoList} from '../reducers/todosSlice';
 
 function TodoGroup() {
 
     const todoListIds = useSelector(selectTodoIds);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        getToDos().then((response) => {
+            dispatch(AddToDoList(response.data));
+        })
+    })
 
     return (
         <div className="todoGroupItems">
