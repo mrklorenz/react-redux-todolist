91f3f67e2a638689415c8d365e41a80f448b4515
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-19 10:26:58 +0800

    [ReactRedux][Mark] feat: Moved getToDos in App.js

--- src/App.js
+++ src/App.js
@@ -6,24 +6,37 @@ import NotFoundPage from './features/todos/components/NotFoundPage';
 import { Breadcrumb } from 'antd';
 import { HomeOutlined, DatabaseOutlined, FileDoneOutlined } from '@ant-design/icons';
 import HomePage from './features/todos/components/HomePage';
+import {useEffect} from "react";
+import { getToDos } from './features/api/todosapi';
+import { useDispatch } from 'react-redux';
+import { AddToDoList } from './features/todos/reducers/todosSlice';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getToDos().then((response) => {
+        dispatch(AddToDoList(response.data));
+    })
+})
+
   return (
     <div className="App">
       <br></br>
-      <BrowserRouter>
-      <Breadcrumb>
-      <Breadcrumb.Item><Link to="/"><HomeOutlined /> HomePage</Link></Breadcrumb.Item>
-      <Breadcrumb.Item><Link to="/todo"><DatabaseOutlined/> To do List Page</Link></Breadcrumb.Item>
-      <Breadcrumb.Item> <Link to="/done"><FileDoneOutlined/> Done List Page</Link></Breadcrumb.Item>
-      </Breadcrumb>
-        <Switch>
-          <Route exact path = "/" component={HomePage}></Route>
-          <Route exact path = "/todo" component={TodoList}></Route>
-          <Route exact path = "/done" component={DoneList}></Route>
-          <Route path = "*" component={NotFoundPage}></Route>
-        </Switch>
-      </BrowserRouter>
+        <Breadcrumb>
+          <BrowserRouter>
+            <Link to="/"><Breadcrumb.Item><HomeOutlined /> HomePage</Breadcrumb.Item></Link>
+            <Link to="/todo"><Breadcrumb.Item><DatabaseOutlined/> To do List Page</Breadcrumb.Item></Link>
+            <Link to="/done"><Breadcrumb.Item><FileDoneOutlined/> Done List Page</Breadcrumb.Item></Link>
+            <Switch>
+              <Route exact path = "/" component={HomePage}></Route>
+              <Route exact path = "/todo" component={TodoList}></Route>
+              <Route exact path = "/done" component={DoneList}></Route>
+              <Route path = "*" component={NotFoundPage}></Route>
+            </Switch>
+          </BrowserRouter>
+        </Breadcrumb>
     </div>
   );
 }
