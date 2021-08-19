b691efd18c4d315d107a2cb450d4f8b9c6ee0dc7
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-20 01:06:31 +0800

    [CORS][Mark] feat: Added edit button

--- src/features/todos/components/TodoItem.js
+++ src/features/todos/components/TodoItem.js
@@ -4,12 +4,16 @@ import {ToggleTodo, DeleteTodo, selectTodoItemId} from '../reducers/todosSlice';
 import "../styles/ToDoItem.css";
 import {DeleteFilled} from '@ant-design/icons';
 import { deleteToDo , updateToDo} from '../../api/todosapi';
+import {EditTwoTone } from '@ant-design/icons';
+import {Input, Modal} from 'antd';
+import { useState } from 'react';
 
 function TodoItem(props) {
 
     const todo = useSelector(state => selectTodoItemId(state, props.id));
-
     const dispatch = useDispatch();
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const[text, setText] = useState("");
 
     const todoStatus = todo.done ? "done" : "";
 
@@ -26,12 +30,38 @@ function TodoItem(props) {
         event.stopPropagation();
     }
 
+    function handleEditClick(event){
+        event.stopPropagation();
+        setIsModalVisible(true);
+    }
+
+    const handleOk = (event) => {
+        event.stopPropagation();
+        setIsModalVisible(false);
+    };
+    
+    const handleCancel = (event) => {
+        event.stopPropagation();
+        setText("");
+        setIsModalVisible(false);
+    };
+
+    function handleEditChange(event){
+        event.stopPropagation();
+        setText(event.target.value);
+    };
+
     return (
         <div className={`toDoFormDiv ${todoStatus}`}  onClick={handleToggle}>
             <span className="textSpan">{todo.text}</span>
             <DeleteFilled className="removeSpan" onClick={handleDeleteTodo}/>
+            <EditTwoTone className="removeSpan" onClick={handleEditClick}/>
+            <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <Input onChange={handleEditChange} value={text}/>
+            </Modal>
         </div>
     );
 }
+
 
 export default TodoItem;
